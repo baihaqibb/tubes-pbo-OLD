6470de936393b1a07993ea1b4f935886ec46a912
baihaqibb <138758831+baihaqibb@users.noreply.github.com>
2024-11-28 20:19:14 +0700

    Initiation

---
+++ src/test/java/com/kelompok2/pbo/PboApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kelompok2.pbo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PboApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
